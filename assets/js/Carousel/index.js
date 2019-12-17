d83ca5bc3857d7c2af49c6a8504a1e593a9bda13
ecl5pse <fantamas20@gmail.com>
2019-12-17 19:00:12 +0200

    finish model

--- assets/js/Carousel/index.js
+++ assets/js/Carousel/index.js
@@ -1,8 +1,61 @@
 "use strict";
 
-export default class  Carousel {
-	constructor () {
+class Carousel {
+
+	constructor (imagesPaths, currentIndex = 0) {
+		if (!Array.isArray(imagesPaths)) {
+			throw  new TypeError();
+		}
+		if (currentIndex < 0 || currentIndex > imagesPaths.length - 1) {
+			throw  new RangeError();
+
+		}
+
+		this.imagesPaths = imagesPaths;
+		this.currentIndex = currentIndex;
+	}
+
+	get currentIndex () {
+
+		return this.currentIndex;
+	}
+
+	set currentIndex (value) {
+
+		if (value < 0 || value > this.length - 1) {
+			throw  new RangeError();
+
+		}
+	}
+
+	get imagesPaths () {
+		return this.imagesPaths;
+	}
+
+	get length () {
+		return this.imagesPaths.length;
+	}
+
+	goNext () {
+		this.currentIndex = Carousel.getNextIndex(this.currentIndex , this.length);
 
 	}
-	
+
+	goPrev () {
+		this.currentIndex = Carousel.getPrevIndex(this.currentIndex , this.length);
+
+	}
+
+	static getNextIndex (index, length) {
+		return (index + 1) % length;
+
+
+	}
+
+	static getPrevIndex (index, length) {
+		return(index - 1 + length) % length;
+	}
+
 }
+
+export default Carousel;
